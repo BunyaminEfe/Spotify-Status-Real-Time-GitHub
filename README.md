7c24eeeb0f82bb1adb8ddef140362b77e9e89bd6
BunyaminEfe <bnefe10@gmail.com>
2021-01-21 22:37:55 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,105 @@
+<h1 align="center">Spotify Status (README)</h1>
+<p align="center">View what you listen at spotify.</p>
+
+<p align="center">
+   <a href="https://github.com/sndrjhlncgr/README-Spotify-Status/issues"><img alt="GitHub issues" src="https://img.shields.io/github/issues/sndrjhlncgr/README-Spotify-Status"></a>
+   <a href="https://github.com/sndrjhlncgr/README-Spotify-Status/network"><img alt="GitHub forks" src="https://img.shields.io/github/forks/sndrjhlncgr/README-Spotify-Status"></a>
+   <a href="https://github.com/sndrjhlncgr/README-Spotify-Status/stargazers"><img alt="GitHub stars" src="https://img.shields.io/github/stars/sndrjhlncgr/README-Spotify-Status"></a>
+   <a href="https://github.com/sndrjhlncgr/README-Spotify-Status/blob/master/LICENSE"><img alt="GitHub license" src="https://img.shields.io/github/license/sndrjhlncgr/README-Spotify-Status"></a>
+<p align="center">
+
+![Image of Sandro Cagara](https://i.ibb.co/Tmjxnhs/README-Spotify-Status-v2.jpg)
+<p align="center">
+   <h3>Demo</h3>
+   <img src="https://spotify-status.vercel.app/api/run-spotify-status" alt="s4nx Playing Now" width="500" />
+<p align="center">
+
+Spotify for Developers
+-----
+
+* Create a [Spotify for Developers](https://developer.spotify.com/dashboard/applications) account
+* Get Credentials
+    * `Client ID`
+    * `Client Secret`
+* Go to **Edit Settings**
+* Find **Redirect URIs**:
+    * Add `http://localhost/callback/`
+
+Get the Refresh Token from Spotify
+-----
+
+* Navigate to the following URL:
+  <br/>
+  **Note**: copy your Client ID and paste in **{SPOTIFY_CLIENT_ID}** below.
+
+```
+https://accounts.spotify.com/authorize?client_id={PUT_YOUR_SPOTIFY_CLIENT_ID_HERE}&response_type=code&scope=user-read-currently-playing,user-read-recently-played&redirect_uri=http://localhost/callback/
+```
+
+* After logging in, get the {GET_THE_TOKEN} portion of: `http://localhost/callback/?code={GET_THE_TOKEN}`
+
+* Create a string combining `{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}` (e.g. `5n7o4v5a3t7o5r2e3m1:5a8n7d3r4e2w5n8o2v3a7c5`) and encode into [Base64](https://www.base64encode.org/).
+
+* Then run a [curl command](https://reqbin.com/curl):
+```sh
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: Basic {YOUR_BASE64}" -d "grant_type=authorization_code&redirect_uri=http://localhost/callback/&code={YOUR_TOKEN}" https://accounts.spotify.com/api/token
+```
+
+* Then save the Refresh token
+
+Configure Vercel Application
+------
+* Fork this [Spotify Status](https://github.com/sndrjhlncgr/Spotify-Status)
+
+* Register on [Vercel](https://vercel.com/)
+
+* Create project linked to your forked respository
+  
+  ![Vercel](https://i.ibb.co/sHhywHD/dasddas.jpg)
+
+* Add Project Name and Environment Variables:
+  - `SPOTIFY_REFRESH_TOKEN`
+  - `SPOTIFY_CLIENT_ID`
+  - `SPOTIFY_SECRET_ID`
+  - `SPOTIFY_BAR_COLOR` 
+     - `Hex Color Don't Include #`
+  - `SPOTIFY_ENABLE_DURATION` => `Not Working properly, set to ```False```
+     - `True or False`
+        
+  ![Vercel](https://i.ibb.co/vv5z4yP/Untitled.png)
+  
+ * Deploy
+
+Put this in your README.md
+------
+<h3>Small</h3>
+
+<img src="https://spotify-status.vercel.app/api/run-spotify-status" alt="s4nx Playing Now" width="350" />
+
+``` 
+[<img src="https://{DOMAIN_OF_YOUR_VERCEL_APP}/api/run-spotify-status" alt="Your alt what" width="350" />](LINK_TO_YOUR_ACCOUNT)
+```
+
+<h3>Medium</h3>
+
+<img src="https://spotify-status.vercel.app/api/run-spotify-status" alt="s4nx Playing Now" width="400" />
+
+``` 
+[<img src="https://{DOMAIN_OF_YOUR_VERCEL_APP}/api/run-spotify-status" alt="Your alt what" width="400" />](LINK_TO_YOUR_ACCOUNT)
+```
+
+<h3>Large</h3>
+
+<img src="https://spotify-status.vercel.app/api/run-spotify-status" alt="s4nx Playing Now" width="500" />
+
+``` 
+[<img src="https://{DOMAIN_OF_YOUR_VERCEL_APP}/api/run-spotify-status" alt="Your alt what" width="500" />](LINK_TO_YOUR_ACCOUNT)
+```
+
+Contribution
+------
+Feel Free to contribute, PR are the most welcome :)
+
+License
+------
+Copyright (c) 2020 Sandro Cagara | Spotify Status
---
+++ api/run-spotify-status.py
@@ -0,0 +1,157 @@
+from flask import Flask, Response, render_template
+from base64 import b64encode
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+import requests, json, os, random
+
+SPOTIFY_URL_REFRESH_TOKEN = "https://accounts.spotify.com/api/token"
+SPOTIFY_URL_NOW_PLAYING = "https://api.spotify.com/v1/me/player/currently-playing"
+SPOTIFY_URL_RECENTLY_PLAY = "https://api.spotify.com/v1/me/player/recently-played?limit=10"
+
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_SECRET_ID = os.getenv("SPOTIFY_SECRET_ID")
+SPOTIFY_REFRESH_TOKEN = os.getenv("SPOTIFY_REFRESH_TOKEN")
+SPOTIFY_BAR_COLOR = os.getenv("SPOTIFY_BAR_COLOR")
+SPOTIFY_ENABLE_DURATION = os.getenv("SPOTIFY_ENABLE_DURATION")
+
+app = Flask(__name__, template_folder="components")
+
+
+def getAuth():
+    return b64encode(f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_SECRET_ID}".encode()).decode("ascii")
+
+
+def refreshToken():
+    data = {
+        "grant_type": "refresh_token",
+        "refresh_token": SPOTIFY_REFRESH_TOKEN,
+    }
+
+    headers = {"Authorization": "Basic {}".format(getAuth())}
+
+    response = requests.post(SPOTIFY_URL_REFRESH_TOKEN, data=data, headers=headers)
+    return response.json()["access_token"]
+
+
+def recentlyPlayed():
+    token = refreshToken()
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(SPOTIFY_URL_RECENTLY_PLAY, headers=headers)
+
+    if response.status_code == 204:
+        return {}
+
+    return response.json()
+
+
+def nowPlaying():
+    token = refreshToken()
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(SPOTIFY_URL_NOW_PLAYING, headers=headers)
+
+    if response.status_code == 204:
+        return {}
+
+    return response.json()
+
+
+def soundVisualizer(soundBars):
+    soundVisualizerCSS = ""
+    START_BAR = 1700  # default: 1700
+    ANIMATIONS = ['animation1', 'animation2', 'animation3']
+    for NTH in range(1, soundBars + 1):
+        START_BAR += 100
+        z = random.randint(0, 2)
+        soundVisualizerCSS += ".spectrograph__bar:nth-child({0}) {{-webkit-animation-name: {1};animation-name: {1};" \
+                              "-webkit-animation-duration: {2}ms;animation-duration: {3}ms;}}".format(NTH,
+                                                                                                      ANIMATIONS[z],
+                                                                                                      str(START_BAR),
+                                                                                                      str(START_BAR))
+    return soundVisualizerCSS
+
+
+def loadImageB64(url):
+    response = requests.get(url)
+    return b64encode(response.content).decode("ascii")
+
+
+def convertMsToMin(ms):
+    seconds = int((ms / 1000) % 60)
+    minutes = int((ms / (1000 * 60)) % 60)
+    return str("%d:%d" % (minutes, seconds))
+
+
+def spectrographWidth():
+    if SPOTIFY_ENABLE_DURATION == 'True':
+        return 52
+    return 98
+
+
+def setSpotifyObject(item):
+    soundBars = 41
+    soundVisualizerBar = "".join(["<div class='spectrograph__bar'></div>" for i in range(soundBars)])
+    soundVisualizerCSS = soundVisualizer(soundBars)
+
+    duration = item["duration_ms"]
+    default_duration = convertMsToMin(duration)
+    musicLink = item["album"]["external_urls"]
+    musicTime = convertMsToMin(item["duration_ms"])
+    explicit = item["explicit"]
+    albumCover = loadImageB64(item["album"]["images"][1]["url"])
+    artistName = item["artists"][0]["name"].replace("&", "&amp;")
+    songName = item["name"].replace("&", "&amp;")
+    spotifyIcon = loadImageB64("https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png")
+    viewAnimation = len(songName) > 27
+
+    spotifyObject = {
+        "enableDuration": SPOTIFY_ENABLE_DURATION,
+        "spectrographWidth": spectrographWidth(),
+        "duration": duration,
+        "default_duration": default_duration,
+        "soundVisualizerBar": soundVisualizerBar,
+        "soundVisualizerCSS": soundVisualizerCSS,
+        "artistName": artistName,
+        "spotifyIcon": spotifyIcon,
+        "viewAnimation":viewAnimation,
+        "songName": songName,
+        "albumCover": albumCover,
+        "barColor": SPOTIFY_BAR_COLOR,
+        "explicit": explicit,
+        "musicTime": musicTime,
+        "musicLink": musicLink
+    }
+    return spotifyObject
+
+
+def makeSVG(data):
+    print(data)
+    if data == {}:
+        recent_plays = recentlyPlayed()
+        size_recent_play = len(recent_plays["items"])
+        idx = random.randint(0, size_recent_play - 1)
+        item = recent_plays["items"][idx]["track"]
+    else:
+        item = data["item"]
+    print(item)  # when ads on its None fix maybe
+
+    spotifyObject = setSpotifyObject(item)
+
+    return render_template("spotifyStatus.html.j2", **spotifyObject)
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+    data = nowPlaying()
+    svg = makeSVG(data)
+
+    resp = Response(svg, mimetype="image/svg+xml")
+    resp.headers["Cache-Control"] = "s-maxage=1"
+
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
